331db427fa3f9bf6409e18c6319335dac9740dd6
Ahmad Khanif Izzaf Arifin <147221263+khanif13@users.noreply.github.com>
2023-10-18 20:20:18 +0800

    Add files via upload

---
+++ Day10PerulanganNo5.java
@@ -0,0 +1,16 @@
+package pkg100.days.coding;
+import java.util.Scanner;
+public class Day10PerulanganNo5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.print("Masukkan nilai n = ");
+        int n = input.nextInt();
+        
+        for (int i = 0; i < n; i++ ){
+            System.out.println("INFORMATIKA");
+        }
+        
+        
+    }
+    
+}
