5d6a4aa9157c85a147c9358a2d5358a154f14c94
Ahmad Khanif Izzaf Arifin <147221263+khanif13@users.noreply.github.com>
2023-10-29 23:02:41 +0800

    Update and rename Day 21 Jumlah Angka Habis Dibagi 3.java to Day 21 Coding War Final.java

--- Day 21 Coding War Final.java
+++ Day 21 Coding War Final.java
@@ -4,11 +4,11 @@ public class Day21 {
     public static void main(String[] args) {
 
         Scanner batas = new Scanner (System.in);
-        System.out.print("Masukkan batas nilai = ");
+        System.out.print("Masukkan batas nilai = ");// Input Batas Nilai
         int batasNilai = batas.nextInt();
         
         for (int i = 1; i < batasNilai; i++) {
-            if (i %3 ==0) {
+            if (i %3 ==0) { // Menentukan modulus sisa bagi
                 System.out.println(i);
             }
         }
