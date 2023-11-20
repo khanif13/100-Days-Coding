44c78a0df3673f3b515eac81e85c85f35214aae8
Ahmad Khanif Izzaf Arifin <147221263+khanif13@users.noreply.github.com>
2023-11-20 19:57:14 +0800

    Add files via upload

---
+++ Day43.java
@@ -0,0 +1,30 @@
+package pkg100.days.coding;
+
+public class Day43 {
+
+    public static void main(String[] args) {
+
+        System.out.println("---------------------");
+        System.out.println("Pola 26");
+        System.out.println("---------------------");
+        System.out.println("7");
+        System.out.println("8");
+        int pola26x = 7;
+        int pola26y = 8;
+
+        for (int i = 1; i <= pola26x; i++) {
+            for (int j = 1; j <= pola26y; j++) {
+                if (true) {
+                    if (i % 2 == 1) {
+                        System.out.print("+ ");
+                    } else {
+                        System.out.print("= ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+}
