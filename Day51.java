01fd37b8a3db9b807e66b9ff9f63a6c035be915d
Ahmad Khanif Izzaf Arifin <147221263+khanif13@users.noreply.github.com>
2023-11-28 21:21:37 +0800

    Add files via upload

---
+++ Day51.java
@@ -0,0 +1,15 @@
+package pkg100.days.coding;
+
+public class Day51 {
+
+    public static void main(String[] args) {
+
+        String[] nama = {"ferdy", "albi", "rina", "robis", "fandi"};
+        
+        for (String i : nama) {
+            System.out.println(i);
+        }
+
+    }
+    
+}
