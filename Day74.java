e97c7ec77003f6c6154eb497581725a7e4f45e59
Ahmad Khanif Izzaf Arifin <147221263+khanif13@users.noreply.github.com>
2023-12-21 21:43:03 +0800

    Add files via upload

---
+++ Day74.java
@@ -0,0 +1,16 @@
+package pkg100.days.coding;
+
+public class Day74 {
+
+    public static void main(String[] args) {
+
+        int [] a = {1, 6, 7, 8, 9, 10};
+        int b = a.length;
+        
+        for (int i = 0; i < b; i++) {
+            System.out.println(a[i]);
+        }
+        
+    }
+    
+}
