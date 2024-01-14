86e2569fb91cab07f56c53d40c29b90bff893648
Ahmad Khanif Izzaf Arifin <147221263+khanif13@users.noreply.github.com>
2024-01-14 20:04:31 +0800

    Add files via upload

---
+++ Day98.java
@@ -0,0 +1,22 @@
+package pkg100.days.coding;
+import java.util.Scanner;
+import ContohClass.Deklarasi;
+
+class Contoh{
+    int a = 5;
+}
+
+public class Day98 {
+
+    public static void main(String[] args) {
+    
+        Contoh obj = new Contoh();
+        ContohObj objCnth = new ContohObj();
+        Deklarasi objDkl = new Deklarasi();
+        
+        System.out.println(obj.a+15);
+        System.out.println(objCnth.nama);
+        System.out.println(objDkl.ttl);
+    }
+    
+}
